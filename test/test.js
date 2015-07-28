accfd0f6082e9842213a98f38937351404b8116c
Michael Barajas <mbarajas@thinknode.com>
2015-07-28 10:47:14 -0400

    Added the request object to wget object, fixed not handling 301 redirects DOH

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 var wget = require('../lib/wget');
 
-var download = wget.download('https://raw.github.com/Fyrd/caniuse/master/data.json', '/tmp/README.md');
+var download = wget.download('https://www.npmjs.com/static/images/npm-logo.svg', '/tmp/README.md');
 // with a proxy:
 // var download = wget.download('https://raw.github.com/Fyrd/caniuse/master/data.json', '/tmp/README.md', {proxy: 'http://proxyhost:port'});
 download.on('error', function(err) {
@@ -8,6 +8,7 @@ download.on('error', function(err) {
 });
 download.on('end', function(output) {
     console.log(output);
+    process.exit();
 });
 download.on('progress', function(progress) {
     console.log(progress);
